0be8007ef5e0775b290ec2dd4d3c6070a0ec0789
Quinn Stobbs <q.stobbs@gmail.com>
2017-08-01 16:54:14 +1000

    sentiment analyser working in the back-end

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -12,39 +12,6 @@ class SearchesController < ApplicationController
 
 
   def show
-
-    @search = Search.new()
-
-    def twitter_config
-
-      client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = "nUm32SXmvBqiJX2g8mDEkew4z"
-      config.consumer_secret     = "3EZHbZ7qbg6rcObEL3QWNwUXaIhqfGJRKgbz9947yCqDOB8MTX"
-      config.access_token        = "316853982-tPm7s630qJqRoh5p4tLSRP4bX8c3irUlvp4FHvs3"
-      config.access_token_secret = "6jRtkivqcQddKyIwvy5Tbh8qdkNIyO0x4NqPBNj9hUSUT"
-    end # client
-
-       search = client.search("to:(params[:q])", :result_type => "recent").take(10).collect do |tweet|
-        puts "#{tweet.user.screen_name}: #{tweet.text}"
-        @username = tweet.user.screen_name
-        @tweet = tweet.text
-      end # client.search
-    #
-    # def search_method
-    #
-    #   @search = Search.new({
-    #     params[:q],
-    #     params[]
-    #     })
-    #
-    # end # search_method
-
-    def results
-      render :json => @tweet
-    end
-
-    end
-
   end # end show
 
   def edit
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,30 @@
 class Tweet < ApplicationRecord
+
+  def self.sync(query)
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = "1BBIstDS2uzQkdcAC4gYosdKk"
+      config.consumer_secret     = "H2Js9WXs4YAQQ5pHzp4PzvkTDdlcg5vYUEAAOJKr9QtjoeTtuj"
+      config.access_token        = "316853982-Rh8nMeqRWhiEGzZi843lelnIweO4o5z8eAuI14W7"
+      config.access_token_secret = "QxDcvQ8mHXb1CHb74vLiI5MQzB9lraazYu8s0ZoPj6MYK"
+    end # client
+
+      #  search = client.search("to:(params[:q])", :result_type => "recent").take(10).collect do |tweet|
+      #   puts "#{tweet.user.screen_name}: #{tweet.text}"
+      #   @username = tweet.user.screen_name
+      #   @tweet = tweet.text
+      # end # client.search
+
+    client.search(query).each do |tweet|
+      create(body: tweet.text)
+    end
+  end
+
+  before_save :set_sentiment, if: :body_changed?
+
+  def set_sentiment
+    self.sentiment = $analyser.sentiment(body)
+    self.score = $analyser.score(body)
+  end
+
+
 end
